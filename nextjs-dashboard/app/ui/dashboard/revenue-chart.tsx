56d41ae3b71af884ed23e7af9f818331e10f9835
WilliansNavas <wiliansn20002@gmail.com>
2025-10-18 01:45:47 -0600

    .

--- nextjs-dashboard/app/ui/dashboard/revenue-chart.tsx
+++ nextjs-dashboard/app/ui/dashboard/revenue-chart.tsx
@@ -25,7 +25,7 @@ export default async function RevenueChart(){
   return (
     <div className="w-full md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Recent Revenue
+        Ingresos Recientes
       </h2>
       NOTE: Uncomment this code in Chapter 7
 
